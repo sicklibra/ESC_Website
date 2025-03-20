ed646b71fcb3a96adb9c2f3a7e941e67ee3a9167
Josh Hodges <sick_libra@hotmail.com>
2025-03-19 21:02:59 -0400

    add favicons, change title

--- ESC_Cabinetry/vite.config.js
+++ ESC_Cabinetry/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  //server:{port <enter desired port # here no<>>,},
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
