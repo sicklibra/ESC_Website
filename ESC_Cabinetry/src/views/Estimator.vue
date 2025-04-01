2dcd6cb1b6f1942740435cf18eae4ea219310e5b
Josh Hodges <sick_libra@hotmail.com>
2025-04-01 12:53:49 -0400

    begin fleshing out funcions in estimator

--- ESC_Cabinetry/src/views/Estimator.vue
+++ ESC_Cabinetry/src/views/Estimator.vue
@@ -4,15 +4,29 @@ export default{
     return{
       kitcostPft: 300,
       bathcostPft: 275,
-      laundcostpft: 200,
+      laundcostPft: 200,
+      closetcostPft: 400,
+
+    };
+  },
+  methods:{
+    disclaimer(){
+      alert('Disclaimer: \n This estimator is for rough estimates only! \n It is intended to give you a very rough estimate of the cost of your project\n and is subject to change drastically.\nThese changes are influenced by but not limited to:/n -Consolidation through a general contractor. \n -various discounts that may be applied.\n -Differing material choices and colors\n -Accessibility hardships and special requirements. \nBy clicking OK you acknowledge that this estimate is in no way binding and subject to change.');
     }
-  }
-}
+    estimate(){
+    }    
+  },
+  beforeMount(){
+      this.disclaimer()
+    },
+};
 </script>
 
 <template>
   <div class="estimator">
-    <h1>This is the estimator page</h1>
+    
+    <h1>Estimator</h1>
+    <p></p>
     <sub>What the hell is happening?</sub>
   </div>
 </template>
