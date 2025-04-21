1c9ec627e80563c9d58662859c0b1746f52bdc44
Josh Hodges <sick_libra@hotmail.com>
2025-04-21 11:57:33 -0400

    populate landing page and format

--- ESC_Cabinetry/src/views/Home.vue
+++ ESC_Cabinetry/src/views/Home.vue
@@ -1,10 +1,32 @@
-<!-- <script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
-</script> -->
+<script setup>
+  
+</script>
 
 <template>
   <div class="home">    
       <h1>Welcome to Example Site Cabinetry</h1>
       <sub>Your escape key to the perfect at home retreat!</sub>
+      <div id="imgbar">
+        <img src="../../public/Images/cabinetimgs/bath/bathtub.jpg" alt="">
+        <img src="../../public/Images/cabinetimgs/kitchen/elegantFarm.JPG" alt="">
+        <img src="../../public/Images/cabinetimgs/kitchen/greenElegant.jpg" alt="">
+      </div>
+      <br>
+      <h3>Hit the escape key on your cabinets.</h3>
+      <p>At Example Site Cabinetry, we custom build your cabinets for your style, your home, your taste, your escape. <br> Customize your experience with unique colors, styles, and accessories designed to make your own home your escape to paradise. All of our cabinets are made taylored for you in our advanced facilities to ensure you have the best product you can get. To top it off, our courteous and professional team of installers will fit your vision into your home bringing your ideas into reality in a clean and professional environement.</p>
   </div>
 </template>
+
+<style scoped> 
+#imgbar img{
+  float: left;
+  width: 32.5%;
+  text-align: center;
+  margin-left: 5px;
+}
+#imgbar{
+  text-align: center;
+  width: 100%;
+  margin: auto;
+}
+</style>
